765dc2da8686ff606cded1a8590c8546b04d07f9
Nikolay <basas1917@gmail.com>
2017-03-14 03:48:09 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,3 +2,7 @@ vk_login = ''       # Логин от сайта vk.com
 vk_password = ''    # Пароль к этому логину
 vk_app_id = -1
 
+# Ключ от cleverbot api
+cleverbot_api_key = ''
+# Адрес комнаты чата
+cleverbot_cs = ''
