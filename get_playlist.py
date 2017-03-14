4fce23136d810f2989ec7e42f4f7ce232a396d7d
Nikolay <basas1917@gmail.com>
2017-03-15 01:56:42 +0300

    get_playlist partical rework
    Added option for getting playlist of similar artists

--- get_playlist.py
+++ get_playlist.py
@@ -1,13 +1,10 @@
 from selenium import webdriver
-from urllib import request
-from urllib.parse import quote
-from bs4 import BeautifulSoup
 from selenium.webdriver.common.keys import Keys
 import requests
 import settings
 import time
 
-# Путь к Crome Driver
+# Путь к Web Driver
 # driver_path = 'C:/Files/Dropbox/Programming/Tools/ChromeDriver/chromedriver.exe'
 # driver_path = 'D:/Dropbox/Programming/Tools/ChromeDriver/chromedriver.exe'
 # driver_path = 'D:/Dropbox/Programming/Tools/phantomjs-2.1.1-windows/bin/phantomjs.exe'
@@ -20,39 +17,53 @@ class VkPlaylist:
         self.vk = vk
         self.item = item
         self.artist = artist
+        print(self.artist)
+        self.artist_formated = artist.split()
         self.driver = None
         self.lastfm_url = ''
-        self.playlist()
 
     # Парсит страницу артиста на сайте last.fm и возвращает список самых популярных композиций этого артиста
     # или 0 в случае, если страницы артиста на данном сайте нет
-    def fetch_top(self, artist):
+    def get_top(self, artist):
         artist_url = "&artist=" + artist
         get_top_url = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks"
-        limit = "&limit=" + "20"
+        limit = "&limit=" + "10"
         top_tracks_url = get_top_url + artist_url + settings.lastfm_api_key + limit + "&format=json"
         top_tracks = requests.get(top_tracks_url).json()
-        self.lastfm_url = top_tracks['toptracks']['track'][1]['artist']['url']
-        print(self.lastfm_url)
+        # print(top_tracks)
         tracks = []
         try:
             for i in top_tracks['toptracks']['track']:
                 tracks.append(i['name'])
-            print(tracks)
             return tracks
         except:
             return 0
 
+    def fetch_top(self, artist):
+        artist_tracks = {artist: self.get_top(artist)}
+        self.lastfm_url = "https://www.last.fm/music/" + "+".join(self.artist_formated)
+        return artist_tracks
 
-    # В ответ на полученное сообщение отправляет плейлист из 10 самых популярных треков указанного исполнителя
-    def playlist(self):
-        song_names = self.fetch_top(self.artist)
-        if song_names == 0:
-            self.vk.respond(self.item, {'message': 'К сожалению, найти треки исполнителя "'
-                                                   + self.artist + '" не удалось.'})
-            return
-        # self.driver = webdriver.Chrome(driver_path)
-        self.driver = webdriver.PhantomJS(driver_path)
+    # Находит 5 похожих исполнителей и отправляет список самыз популярных треков каждого
+    def fetch_similar(self, artist):
+        artist_url = "&artist=" + artist
+        get_similar_url = "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar"
+        limit = "&limit=" + "5"
+        similar_tracks_url = get_similar_url + artist_url + settings.lastfm_api_key + limit + "&format=json"
+        similar_tracks = requests.get(similar_tracks_url).json()
+        self.lastfm_url = "https://www.last.fm/music/" + "+".join(self.artist_formated) + "/+similar"
+        artists = []
+        try:
+            for i in similar_tracks['similarartists']['artist']:
+                artists.append(i['name'])
+            artist_tracks = {}
+            for j in range(5):
+                artist_tracks[artists[j]] = self.get_top(artists[j])
+            return artist_tracks
+        except:
+            return 0
+
+    def get_chat_urls(self):
         if 'chat_id' in self.item:
             attach_url = 'https://m.vk.com/attachments?act=choose_audio&target=mail'\
                          + str(2000000000 + int(self.item['chat_id'])) + '&tab=search'
@@ -61,12 +72,7 @@ class VkPlaylist:
             attach_url = 'https://m.vk.com/attachments?act=choose_audio&target=mail' + str(
                 self.item['user_id']) + '&tab=search'
             chat_url = 'https://m.vk.com/mail?act=show&peer=' + str(self.item['user_id'])
-
-
-        self.vk_authorization()
-        self.send_playlist(self.artist, song_names, attach_url, chat_url)
-        time.sleep(1)
-        self.driver.close()
+        return attach_url, chat_url
 
     # Авторизация на сайте vk.com
     def vk_authorization(self, user_login=settings.vk_login, user_password=settings.vk_password):
@@ -108,7 +114,7 @@ class VkPlaylist:
             pass
 
     # Поиск песен из плейлиста в аудиозаписях контакта и отправка готового плейлиста пользователю
-    def send_playlist(self, artist, song_names, attach_url, chat_url):
+    def send_playlist1(self, artist, song_names, attach_url, chat_url):
         # Добавление аудиозаписей в прикреплённые к сообщению файлы
         i = 10
         for song in song_names:
@@ -125,3 +131,43 @@ class VkPlaylist:
         #                   + self.lastfm_url)
         send = self.driver.find_element_by_xpath("//div[@class='cp_buttons_block']//input[@class='button']")
         send.click()
+
+    def send_songs(self, list_of_songs, attach_url, chat_url, number_of_songs):
+        # Добавление аудиозаписей в прикреплённые к сообщению файлы
+        count_of_attachments = 0
+        for artist in list_of_songs:
+            if count_of_attachments == 10:
+                break
+            i = number_of_songs
+            for song in list_of_songs[artist]:
+                if i == 0:
+                    break
+                t = self.attach_song(artist, song, attach_url)
+                count_of_attachments += 1
+                if t == 1:
+                    i -= 1
+        # Отправка сообщения пользователю
+        self.driver.get(chat_url)
+        message = self.driver.find_element_by_xpath("//textarea[@name='message']")
+        message.send_keys(self.lastfm_url)
+        send = self.driver.find_element_by_xpath("//div[@class='cp_buttons_block']//input[@class='button']")
+        send.click()
+
+    def send_playlist(self, similar=False):
+        if similar:
+            list_of_songs = self.fetch_similar(self.artist)
+        else:
+            list_of_songs = self.fetch_top(self.artist)
+        if list_of_songs == 0:
+            self.vk.respond(self.item, {'message': 'К сожалению, найти треки исполнителя "'
+                                                   + self.artist + '" не удалось.'})
+            return
+        self.driver = webdriver.PhantomJS(driver_path)
+        attach_url, chat_url = self.get_chat_urls()
+        self.vk_authorization()
+        if len(list_of_songs) != 1:
+            self.send_songs(list_of_songs, attach_url, chat_url, 2)
+        else:
+            self.send_songs(list_of_songs, attach_url, chat_url, 10)
+        time.sleep(1)
+        self.driver.close()
--- message_parse.py
+++ message_parse.py
@@ -10,19 +10,24 @@ def parse(vk, item):
     try:
         if item['body']:
             message = item['body']
-            if re.match(r'!p ', message) or re.match(r'@p ', message):
-                artist = re.findall(r'!p (.*)|@p (.*)', message)[0]
-                for i in artist:
-                    if i:
-                        artist = i
-                        break
+            if re.match(r'!p ', message):
+                artist = re.findall(r'!p (.*)', message)[0]
                 print('Sending playlist...')
-                VkPlaylist(vk, item, artist)
+                playlist = VkPlaylist(vk, item, artist)
+                playlist.send_playlist()
+                print('Playlist sent!')
+                return
+            if re.match(r'!s ', message):
+                artist = re.findall(r'!s (.*)', message)[0]
+                print('Sending playlist of similar music...')
+                playlist = VkPlaylist(vk, item, artist)
+                playlist.send_playlist(similar=True)
                 print('Playlist sent!')
                 return
             if 'chat_id' in item:
                 if re.match(r'!c ', message) or (re.match(r'!вов ', message)):
-                    message = re.findall(r'!c (.*)|!вов (.*)', message)[0]
+                    message = re.findall(r'!c (.+)|!вов (.+)', message)[0]
+                    # print(message)
                     for i in message:
                         if i:
                             message = i
