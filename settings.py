e666a234b175f7dd5b36022d1a92ab328017d969
Nikolay Grishachev <basas1917@gmail.com>
2017-03-18 04:48:29 +0300

    fixed untracked files

--- settings.py
+++
@@ -1,9 +0,0 @@
-vk_login = ''       # Логин от сайта vk.com
-vk_password = ''    # Пароль к этому логину
-
-# Ключ от cleverbot api
-cleverbot_api_key = ''
-# Адрес комнаты чата
-cleverbot_cs = ''
-# Ключ от last.fm api
-lastfm_api_key = ''
